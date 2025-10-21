89262aa18c215dc5e7ac5c281947c1834fb79574
Kelvin Junior <105466232+kelvinjunior77@users.noreply.github.com>
2025-10-22 00:25:07 +0200

    menu-navbar

--- resources/js/Pages/Dashboard/NavBar.vue
+++ resources/js/Pages/Dashboard/NavBar.vue
@@ -32,14 +32,13 @@ import logoLight from '@images/favicon.svg';
                         <label>Usuarios</label>
                         <i data-feather="feather"></i>
                     </li>
-                     <li class="pc-item pc-hasmenu">
+                    <li class="pc-item pc-hasmenu">
                         <a href="#!" class="pc-link"><span class="pc-micon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-text-align-justify-icon lucide-text-align-justify"><path d="M3 5h18"/><path d="M3 12h18"/><path d="M3 19h18"/></svg>
-
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round-plus-icon lucide-user-round-plus"><path d="M2 21a8 8 0 0 1 13.292-6"/><circle cx="10" cy="8" r="5"/><path d="M19 16v6"/><path d="M22 19h-6"/></svg>
                             </span><span class="pc-mtext">Cadastrar</span><span class="pc-arrow">
-                                
-                                
-                                </span></a>
+
+
+                            </span></a>
                         <ul class="pc-submenu">
                             <li class="pc-item"><a class="pc-link" href="#!">Usuario</a></li>
                             <!--- <li class="pc-item pc-hasmenu">
@@ -68,45 +67,76 @@ import logoLight from '@images/favicon.svg';
                     </li>
                     <li class="pc-item pc-hasmenu">
                         <a href="../elements/bc_color.html" class="pc-link">
-                            <span class="pc-micon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-users-round-icon lucide-users-round"><path d="M18 21a8 8 0 0 0-16 0"/><circle cx="10" cy="8" r="5"/><path d="M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3"/></svg></span>
+                            <span class="pc-micon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round"
+                                    class="lucide lucide-users-round-icon lucide-users-round">
+                                    <path d="M18 21a8 8 0 0 0-16 0" />
+                                    <circle cx="10" cy="8" r="5" />
+                                    <path d="M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3" />
+                                </svg></span>
 
                             <span class="pc-mtext">Desenvolvedores</span>
                         </a>
                     </li>
-                    <li class="pc-item pc-hasmenu">
-                        <a href="../elements/bc_typography.html" class="pc-link">
-                            <span class="pc-micon"> <i data-feather="type"></i></span>
-                            <span class="pc-mtext">Typography</span>
-                        </a>
-                    </li>
-                    <li class="pc-item pc-hasmenu">
-                        <a href="../elements/icon-feather.html" class="pc-link">
-                            <span class="pc-micon"> <i data-feather="feather"></i></span>
-                            <span class="pc-mtext">Icons</span>
-                        </a>
-                    </li>
+
 
                     <li class="pc-item pc-caption">
-                        <label>Pages</label>
+                        <label>Projetos</label>
                         <i data-feather="monitor"></i>
                     </li>
                     <li class="pc-item pc-hasmenu">
-                        <a href="../pages/login-v1.html" class="pc-link" target="_blank">
-                            <span class="pc-micon"> <i data-feather="lock"></i></span>
-                            <span class="pc-mtext">Login</span>
+                        <a href="#!" class="pc-link"><span class="pc-micon">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-folder-kanban-icon lucide-folder-kanban"><path d="M4 20h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.93a2 2 0 0 1-1.66-.9l-.82-1.2A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13c0 1.1.9 2 2 2Z"/><path d="M8 10v4"/><path d="M12 10v2"/><path d="M16 10v6"/></svg>
+
+                            </span><span class="pc-mtext">Cadastrar</span><span class="pc-arrow">
+
+
+                            </span></a>
+                        <ul class="pc-submenu">
+                            <li class="pc-item"><a class="pc-link" href="#!">Projeto</a></li>
+                            <li class="pc-item"><a class="pc-link" href="#!">Tarefa</a></li>
+                            <!--- <li class="pc-item pc-hasmenu">
+
+                                <a href="#!" class="pc-link">Level 2.2<span class="pc-arrow"><i
+                                            class="ti ti-chevron-right"></i></span></a>
+                            </li>
+                           
+                            <li class="pc-item pc-hasmenu">
+                                <a href="#!" class="pc-link">Level 2.3<span class="pc-arrow"><i
+                                            class="ti ti-chevron-right"></i></span></a>
+                                <ul class="pc-submenu">
+                                    <li class="pc-item"><a class="pc-link" href="#!">Level 3.1</a></li>
+                                    <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
+                                    <li class="pc-item pc-hasmenu">
+                                        <a href="#!" class="pc-link">Level 3.3<span class="pc-arrow"><i
+                                                    class="ti ti-chevron-right"></i></span></a>
+                                        <ul class="pc-submenu">
+                                            <li class="pc-item"><a class="pc-link" href="#!">Level 4.1</a></li>
+                                            <li class="pc-item"><a class="pc-link" href="#!">Level 4.2</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>---->
+                        </ul>
+                    </li>
+                        <li class="pc-item pc-hasmenu">
+                        <a href="../pages/register-v1.html" class="pc-link" target="_blank">
+                            <span class="pc-micon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-folder-git2-icon lucide-folder-git-2"><path d="M9 20H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h3.9a2 2 0 0 1 1.69.9l.81 1.2a2 2 0 0 0 1.67.9H20a2 2 0 0 1 2 2v5"/><circle cx="13" cy="12" r="2"/><path d="M18 19c-2.8 0-5-2.2-5-5v8"/><circle cx="20" cy="19" r="2"/></svg></span>
+                            <span class="pc-mtext">Projetos</span>
                         </a>
                     </li>
                     <li class="pc-item pc-hasmenu">
                         <a href="../pages/register-v1.html" class="pc-link" target="_blank">
-                            <span class="pc-micon"> <i data-feather="user-plus"></i></span>
-                            <span class="pc-mtext">Register</span>
+                            <span class="pc-micon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-clipboard-list-icon lucide-clipboard-list"><rect width="8" height="4" x="8" y="2" rx="1" ry="1"/><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/><path d="M12 11h4"/><path d="M12 16h4"/><path d="M8 11h.01"/><path d="M8 16h.01"/></svg></span>
+                            <span class="pc-mtext">Tarefas</span>
                         </a>
                     </li>
                     <li class="pc-item pc-caption">
                         <label>Other</label>
                         <i data-feather="sidebar"></i>
                     </li>
-                   
+
                     <li class="pc-item">
                         <a href="../other/sample-page.html" class="pc-link">
                             <span class="pc-micon">
