4c9070f8f08ed6b597f369e6760ee484e2163098
Kelvin Junior <105466232+kelvinjunior77@users.noreply.github.com>
2026-01-03 00:42:27 +0100

    .

--- resources/js/Pages/Public/Tarefas/TarefaEdit.vue
+++ resources/js/Pages/Public/Tarefas/TarefaEdit.vue
@@ -33,15 +33,61 @@ const submit = () => {
     <Layout>
         <div class="w-full px-6 py-6">
 
-            <!-- Header -->
-            <div class="mb-6">
-                <h1 class="text-3xl font-bold text-primary">
-                    Editar Tarefa
-                </h1>
-                <p class="text-base-content/70 mt-1">
-                    Projeto:
-                    <strong>{{ tarefa.projeto?.nome }}</strong>
-                </p>
+            <div class="max-w-10xl h-10 mb-10">
+                <div
+                    class="flex flex-col md:flex-row md:items-center justify-between bg-base-100 border border-base-200 rounded-xl px-1 py-1 shadow-sm">
+                    <!-- Título -->
+                    <div>
+
+                        <!-- Breadcrumb -->
+                        <nav class="text-sm breadcrumbs mt-1 p-2 opacity-70">
+                            <ul>
+                                <li>
+                                    <Link href="/">
+                                        Home
+                                    </Link>
+                                </li>
+
+                                <li class="font-medium text-primary">
+                                    Lista de Tarefas
+                                </li>
+
+                                <li class="font-medium text-primary">
+                                    Editar Tarefa
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+
+
+                </div>
+            </div>
+
+            <div v-if="$page.props.flash.error" role="alert" class="alert alert-error mb-3">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none"
+                    viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span>{{ $page.props.flash.error }}</span>
+            </div>
+
+            <div class="mb-5 ">
+
+                <div role="alert" class="alert alert-vertical sm:alert-horizontal">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                        class="lucide lucide-folder-root-icon lucide-folder-root">
+                        <path
+                            d="M4 20h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.93a2 2 0 0 1-1.66-.9l-.82-1.2A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13c0 1.1.9 2 2 2Z" />
+                        <circle cx="12" cy="13" r="2" />
+                        <path d="M12 15v5" />
+                    </svg>
+                    <span class="font-bold">{{ projeto.nome }}</span>
+                    <div>
+                        <button @click="abrirModal(projeto)" class="btn btn-sm btn-info">Ver</button>
+                    </div>
+                </div>
             </div>
 
             <!-- Conteúdo principal -->
@@ -51,10 +97,11 @@ const submit = () => {
                 <div class="card bg-base-100 shadow-md border border-base-300">
                     <div class="card-body space-y-5">
 
-                        <!-- Título -->
+
                         <div>
                             <label class="label font-semibold">Título</label>
-                            <input v-model="form.titulo" type="text" class="input input-bordered w-full" />
+                            <input v-model="form.titulo" type="text"
+                                class="input input-bordered w-full transition-all duration-100 outline-0 focus:border-blue-900 focus:border-2" />
                             <p v-if="form.errors.titulo" class="text-error text-sm">
                                 {{ form.errors.titulo }}
                             </p>
@@ -63,13 +110,15 @@ const submit = () => {
                         <!-- Descrição -->
                         <div>
                             <label class="label font-semibold">Descrição</label>
-                            <textarea v-model="form.descricao" rows="4" class="textarea textarea-bordered w-full" />
+                            <textarea v-model="form.descricao" rows="4"
+                                class="textarea textarea-bordered w-full transition-all duration-100 outline-0 focus:border-blue-900 focus:border-2" />
                         </div>
 
                         <!-- Função -->
                         <div>
                             <label class="label font-semibold">Função</label>
-                            <textarea v-model="form.funcao" rows="3" class="textarea textarea-bordered w-full" />
+                            <textarea v-model="form.funcao" rows="3" class="textarea textarea-bordered w-full 
+                            transition-all duration-100 outline-0 focus:border-blue-900 focus:border-2" />
                         </div>
 
                     </div>
@@ -85,7 +134,8 @@ const submit = () => {
                             <!-- Status -->
                             <div>
                                 <label class="label font-semibold">Status</label>
-                                <select v-model="form.status" class="select select-bordered w-full">
+                                <select v-model="form.status" class="select select-bordered w-full
+                                transition-all duration-100 outline-0 focus:border-blue-900 focus:border-2 ">
                                     <option value="pendente">Pendente</option>
                                     <option value="em_andamento">Em andamento</option>
                                     <option value="concluido">Concluído</option>
@@ -96,7 +146,8 @@ const submit = () => {
                             <!-- Prioridade -->
                             <div>
                                 <label class="label font-semibold">Prioridade</label>
-                                <select v-model="form.prioridade" class="select select-bordered w-full">
+                                <select v-model="form.prioridade" class="select select-bordered w-full 
+                                transition-all duration-100 outline-0 focus:border-blue-900 focus:border-2">
                                     <option value="baixa">Baixa</option>
                                     <option value="media">Média</option>
                                     <option value="alta">Alta</option>
@@ -106,7 +157,8 @@ const submit = () => {
                             <!-- Data final -->
                             <div>
                                 <label class="label font-semibold">Data final</label>
-                                <input v-model="form.data_fim" type="date" class="input input-bordered w-full" />
+                                <input v-model="form.data_fim" type="date" class="input input-bordered w-full 
+                                transition-all duration-100 outline-0 focus:border-blue-900 focus:border-2" />
                             </div>
 
                         </div>
@@ -118,8 +170,8 @@ const submit = () => {
                             </label>
 
                             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
-                                <label v-for="user in usuarios" :key="user.id" class="flex items-center gap-3 p-3 border rounded-xl cursor-pointer
-                   hover:bg-base-200 transition">
+                                <label v-for="user in usuarios" :key="user.id" class="flex items-center gap-3 p-3 
+                                border border-base-300 rounded-xl cursor-pointer hover:bg-base-200 transition">
                                     <!-- Checkbox -->
                                     <input type="checkbox" class="checkbox checkbox-primary" :value="user.id"
                                         v-model="form.usuarios" />
@@ -127,13 +179,12 @@ const submit = () => {
                                     <!-- Avatar -->
                                     <div class="avatar">
                                         <div class="w-9 h-9 rounded-full ring ring-base-300">
-                                            <img v-if="user.avatar" :src="`/storage/${user.avatar}`"
-                                                :alt="user.name" />
+                                            <img v-if="user.avatar" :src="`/storage/${user.avatar}`" :alt="user.name" />
 
                                             <div v-else
                                                 class="flex items-center justify-center bg-primary text-primary-content font-bold">
                                                 {{ user.name.charAt(0).toUpperCase() }}
-                                                
+
                                             </div>
                                         </div>
                                     </div>
