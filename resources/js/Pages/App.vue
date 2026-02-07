d956192e1a6eeacce7e2ac06fffc2659f289589c
Kelvin Junior <105466232+kelvinjunior77@users.noreply.github.com>
2026-02-07 22:43:34 +0100

    props

--- resources/js/Pages/App.vue
+++ resources/js/Pages/App.vue
@@ -1,8 +1,15 @@
 <template>
-    <Dashboard>
-    </Dashboard>
+    <Dashboard :projetos="projectsCount" :tarefas="tasksCount" :projetosFinalizados="finishedProjectsCount" :tarefasFeitas="finishedTasksCount" />
 </template>
 
 <script setup>
 import Dashboard from './Dashboard/Dashboard.vue';
+
+defineProps({
+    projectsCount: Number,
+    tasksCount: Number,
+    finishedProjectsCount: Number,
+    finishedTasksCount: Number,
+    
+})
 </script>
