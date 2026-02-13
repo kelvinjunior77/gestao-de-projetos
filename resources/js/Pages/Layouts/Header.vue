9f1129219eeaaa971e63e525de7775f186a3ecf9
Kelvin Junior <105466232+kelvinjunior77@users.noreply.github.com>
2026-02-13 23:54:41 +0100

    perfil link

--- resources/js/Pages/Layouts/Header.vue
+++ resources/js/Pages/Layouts/Header.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { Link, usePage, router } from '@inertiajs/vue3';
+import { route } from "ziggy-js";
 
 // Dados do usuário
 const page = usePage();
@@ -88,7 +89,8 @@ function logout() {
       <button class="btn btn-ghost btn-circle" @click="toggleTheme" title="Mudar Tema">
         <!-- Ícone do Sol (tema claro) -->
 
-        <svg v-if="currentTheme === 'light'" class="swap-off h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <svg v-if="currentTheme === 'light'" class="swap-off h-6 w-6 fill-current" xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24">
           <path
             d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
         </svg>
@@ -126,22 +128,42 @@ function logout() {
       </div>
 
       <!-- USER DROPDOWN -->
-      <div class="dropdown dropdown-end" @click.stop="closeAll(); dProfile = !dProfile">
+      <div class="dropdown dropdown-end bg-base-200" @click.stop="closeAll(); dProfile = !dProfile">
         <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
           <div class="w-10 rounded-full">
-              <img v-if="user.avatar" :src="user.avatar" alt="Avatar do usuario" />
+            <img v-if="user.avatar" :src="user.avatar" alt="Avatar do usuario" />
           </div>
         </div>
 
-        <ul v-show="dProfile" class="dropdown-content z-50 mt-3 w-56 bg-base-100 border rounded-box shadow p-2">
-          <li class="px-3 py-2 border-b">
-            <div class="font-bold">{{ user.name }}</div>
-            <div class="text-xs opacity-70">{{ user.email }}</div>
+        <ul v-show="dProfile" class="dropdown-content z-50 mt-3 w-56 bg-base-200 rounded-box shadow p-2">
+          <li class="px-3 py-2 text-center">
+            <div class="font-bold mb-2">{{ user.name }}</div>
+            <div class="text-xs opacity-70 mb-2">
+              <span class="badge badge-soft badge-primary">{{ user.email }}</span>
+            </div>
+            <div class="text-xs opacity-70 mt-1">
+              <span class="badge badge-soft badge-primary">{{ user.tipo }}</span>
+            </div>
           </li> <br>
-          <li><a href="" class="">Minha Conta</a></li>
-          <li>
-            <Link method="post" href="/logout" class="text-error cursor-pointer mt-3">Sair</Link>
-          </li>
+
+          <div class="flex items-center flex-col">
+            <li>
+              <Link class="btn btn-sm" :href="(route('usuario.perfil', user.slug))">Meu Perfil</Link>
+            </li>
+            <li class="mt-2">
+
+              <Link method="post" href="/logout" class="text-error cursor-pointer mt-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                  class="lucide lucide-power-icon lucide-power">
+                  <path d="M12 2v10" />
+                  <path d="M18.4 6.6a9 9 0 1 1-12.77.04" />
+                </svg>
+
+              </Link>
+            </li>
+          </div>
+
         </ul>
 
       </div>
